ea1d02e3b8f39829954f1aad4614895157878252
Lucas Postiglioni Obes <73368@proway.treina>
2023-06-24 13:48:54 -0300

    marcos

--- WindowsFormsExemplos/Forms/Produtos/ListagemProdutoForm.cs
+++ WindowsFormsExemplos/Forms/Produtos/ListagemProdutoForm.cs
@@ -13,6 +13,8 @@ namespace WindowsFormsExemplos.Forms.Produtos
 {
     public partial class ListagemProdutoForm : Form
     {
+        private ProdutoServico produtoServico;
+
         public ListagemProdutoForm()
         {
             InitializeComponent();
@@ -21,6 +23,8 @@ namespace WindowsFormsExemplos.Forms.Produtos
         private void ListagemProdutoForm_Load(object sender, EventArgs e)
         {
             ListarProdutos();
+
+            produtoServico = new ProdutoServico();
         }
 
         private void ListarProdutos()
@@ -30,7 +34,6 @@ namespace WindowsFormsExemplos.Forms.Produtos
 
 
             // Obter a lista de produtos
-            var produtoServico = new ProdutoServico();
             var produtos = produtoServico.ObterTodos(pesquisa);
 
             // Remover linhas do dataGridView
@@ -66,9 +69,6 @@ namespace WindowsFormsExemplos.Forms.Produtos
             // Obter o valor da primeira coluna (código == id) da linha selecionada
             var id = Convert.ToInt32(linhaSelecionada.Cells[0].Value);
 
-            // Instanciando um objeto da class ProdutoServico
-            var produtoServico = new ProdutoServico();
-
             // Chamar o método que irá realizar o DELETE
             produtoServico.Apagar(id);
 
@@ -81,7 +81,6 @@ namespace WindowsFormsExemplos.Forms.Produtos
             var linhaSelecionada = dataGridView1.SelectedRows[0];
             var id = Convert.ToInt32(linhaSelecionada.Cells[0].Value);
 
-            var produtoServico = new ProdutoServico();
             var produtoEscolhido = produtoServico.ObterPorId(id);
 
             var form = new CadastroProdutoForm(produtoEscolhido);
--- WindowsFormsExemplos/Servicos/ProdutoServico.cs
+++ WindowsFormsExemplos/Servicos/ProdutoServico.cs
@@ -9,17 +9,16 @@ namespace WindowsFormsExemplos.Servicos
 {
     public class ProdutoServico
     {
+        private ProdutoRepositorio produtoRepositorio;
         // CRUD
         public void Cadastrar(string nome, decimal precoUnitario, int quantidade)
         {
-            var produtoRepositorio = new ProdutoRepositorio();
             produtoRepositorio.Cadastrar(nome, precoUnitario, quantidade);
         }
 
         public List<Produto> ObterTodos(string pesquisa)
         {
             // Obter a lista de produtos da tabela de produtos
-            var produtoRepositorio = new ProdutoRepositorio();
             var produtos = produtoRepositorio.ObterTodosProdutos(pesquisa);
 
             // Retornar a lista de produtos
@@ -28,22 +27,18 @@ namespace WindowsFormsExemplos.Servicos
 
         public void Apagar(int id)
         {
-            // Instanciando um objeto da classe ProdutoRepositorio
-            var produtoRepositorio = new ProdutoRepositorio();
             // Chamar o método Apagar do ProdutoRepositorio (executa o DELETE)
             produtoRepositorio.Apagar(id);
         }
 
         public Produto ObterPorId(int id)
         {
-            var produtoRepositorio = new ProdutoRepositorio();
             var produto = produtoRepositorio.ObterPorId(id);
             return produto;
         }
 
         internal void Editar(int idProdutoEditar, string nome, decimal precoUnitario, int quantidade)
         {
-            var produtoRepositorio = new ProdutoRepositorio();
             produtoRepositorio.Editar(idProdutoEditar, nome, precoUnitario, quantidade);
         }
     }
